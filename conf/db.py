d2ff455766c35105758c200e9a3ad884a2499224
Tom <332138569@qq.com>
2016-10-19 10:36:53 +0800

    finished

--- conf/db.py
+++ conf/db.py
@@ -31,3 +31,75 @@ class _DbCtx(threading.local):
 		return self.connection.cursor()
 
 _db_ctx = _DbCtx()
+
+class _ConnectionCtx(object):
+	def __enter__(self):
+		global _db_ctx
+		self.should_cleanup = False
+		if not _db_ctx.is_init():
+			_db_ctx.init()
+			self.should_cleanup = True
+		return self
+
+	def __exit__(self, excvalue, traceback):
+		global _db_ctx
+		if self.should_cleanup:
+			_db_ctx.cleanup()
+
+def connection():
+	return _ConnectionCtx()
+
+with connection():
+	d0_some_db_operation():
+		pass
+
+@with_connection
+def select(sql, *args):
+	pass
+
+@with_connection
+def update(sql, *args):
+	pass
+
+with db.transactions():
+	db.select('...')
+	db.update('...')
+	db.update('...')
+
+class _transactionCtx(object):
+	def __enter__(self):
+		global _db_ctx
+		self.shoule_close_conn = False
+		if not _db_ctx.is_init():
+			_db_ctx.init()
+			self.should_close_conn = True
+		_db_ctx.transactions = _db_ctx.transactions + 1
+		return self
+
+	def __exit__(self, exctype, excvalue, traceback):
+		global _db_ctx
+		_db_ctx.transactions = _db_ctx.transactions - 1
+		try:
+			if _db_ctx.transactions == 0:
+				if sxctype is None:
+					self.commit()
+				else:
+					self.rolback()
+		finally:
+			if self.should_close_conn:
+				_db_ctx.cleanup()
+
+	def commit(self):
+		global _db_ctx
+		try:
+			_db_ctx.connection.commit()
+		except:
+			_db_ctx.connection.rollback()
+			raise
+
+	def rollback(self):
+		global _db_ctx
+		_db_ctx.connection.rollback()
+		
+
+
